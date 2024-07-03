6fe711b478f97d40f6e3c9c2956ceeb0fbe81011
Paktakraw <tresarpnat27@gmail.com>
2024-07-03 13:06:53 +0700

    exercise algo nested loop complete

--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,15 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(n) {
+  let finalResult = "";
+
+  for (let i = 1; i <= n; i++) {
+    let rowResult = "";
+    for (let j = 1; j <= n; j++) {
+      rowResult = rowResult + j * i + "\t";
+    }
+    finalResult = finalResult + rowResult + "\n";
+  }
+  return finalResult;
 }
+
+console.log(multiplicationTable(5));
